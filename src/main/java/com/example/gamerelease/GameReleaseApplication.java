f2fafdfdd99d267ad308396f78ff1cc9e8320c8b
tenma.yamazaki <tenma.yamazaki@ari-jp.com>
2018-07-25 15:51:15 +0900

    first commite

---
+++ src/main/java/com/example/gamerelease/GameReleaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.gamerelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameReleaseApplication.class, args);
+	}
+}
