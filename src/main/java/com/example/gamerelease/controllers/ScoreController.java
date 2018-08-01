265a2cda5528ea1d744e6604e378aea0424f1f98
tenma.yamazaki <tenma.yamazaki@ari-jp.com>
2018-08-01 09:38:31 +0900

    名前登録時に未入力だった場合ランダムに50代俳優名で登録される仕様に変更

--- src/main/java/com/example/gamerelease/controllers/ScoreController.java
+++ src/main/java/com/example/gamerelease/controllers/ScoreController.java
@@ -32,7 +32,19 @@ public class ScoreController {
     public Score addrank(@RequestBody AddScore addScore) {
         String name = addScore.getName();
         if(name.equals("")) {
-            name = "anonymous";
+            int rand = (int)(Math.random() * 5);
+            switch (rand) {
+                case 0 : name = "香川照之.";
+                         break;
+                case 1 : name = "阿部寛.";
+                         break;
+                case 2 : name = "吉田鋼太郎.";
+                         break;
+                case 3 : name = "堤真一.";
+                         break;
+                default : name = "哀川翔.";
+                         break;
+            }
         }
         int score = addScore.getScore();
         String gameCode = addScore.getGamecode();
